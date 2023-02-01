45fe6286580585f910c049e27dc15125311ab2ac
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-01 14:45:29 -0300

    Adicionado PessoaService com métodos de conversão e cadastro

---
+++ src/main/java/com/attornatus/pessoaapi/service/PessoaService.java
@@ -0,0 +1,45 @@
+package com.attornatus.pessoaapi.service;
+
+import com.attornatus.pessoaapi.dto.pessoadto.PessoaCreateDTO;
+import com.attornatus.pessoaapi.dto.pessoadto.PessoaDTO;
+import com.attornatus.pessoaapi.entities.PessoaEntity;
+import com.attornatus.pessoaapi.repository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PessoaService {
+
+    private PessoaRepository pessoaRepository;
+
+    public PessoaDTO cadastrarPessoa(PessoaCreateDTO pessoaCreateDTO) {
+        PessoaEntity pessoa = converterEmPessoaEntity(pessoaCreateDTO);
+
+        pessoa.setNome(pessoaCreateDTO.getNome());
+        pessoa.setDataNascimento(pessoaCreateDTO.getDataNascimento());
+
+        pessoaRepository.save(pessoa);
+
+        return converterEmPessoaDTO(pessoa);
+    }
+
+
+    public PessoaEntity converterEmPessoaEntity (PessoaCreateDTO pessoa) {
+        PessoaEntity pessoaEntity = new PessoaEntity();
+        pessoaEntity.setIdPessoa(pessoaEntity.getIdPessoa());
+        pessoaEntity.setNome(pessoaEntity.getNome());
+        pessoaEntity.setDataNascimento(pessoa.getDataNascimento());
+
+        return pessoaEntity;
+    }
+
+    public PessoaDTO converterEmPessoaDTO (PessoaEntity pessoa) {
+        PessoaDTO pessoaDTO = new PessoaDTO();
+        pessoaDTO.setIdPessoa(pessoa.getIdPessoa());
+        pessoaDTO.setNome(pessoa.getNome());
+        pessoaDTO.setDataNascimento(pessoa.getDataNascimento());
+
+        return pessoaDTO;
+    }
+}
