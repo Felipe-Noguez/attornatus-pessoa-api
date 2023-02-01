8068cb4cc6d1f7a454ffe0a20759f5a1ef216196
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-01 19:53:43 -0300

    Adicionado consulta por id de uma pessoa

--- src/main/java/com/attornatus/pessoaapi/controller/PessoaController.java
+++ src/main/java/com/attornatus/pessoaapi/controller/PessoaController.java
@@ -2,6 +2,7 @@ package com.attornatus.pessoaapi.controller;
 
 import com.attornatus.pessoaapi.dto.pessoadto.PessoaCreateDTO;
 import com.attornatus.pessoaapi.dto.pessoadto.PessoaDTO;
+import com.attornatus.pessoaapi.exception.RegraDeNegocioException;
 import com.attornatus.pessoaapi.service.PessoaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,12 +34,21 @@ public class PessoaController {
     @PutMapping("/atualizar-pessoa")
     public ResponseEntity<PessoaDTO> atualizarPessoa (@Valid
                                                       Integer idPessoa,
-                                                      @RequestBody PessoaCreateDTO pessoaCreateDTO) {
+                                                      @RequestBody PessoaCreateDTO pessoaCreateDTO) throws RegraDeNegocioException {
         log.info("Editando cadastro do usuário");
         PessoaDTO pessoaDTO = pessoaService.editarPessoa(idPessoa, pessoaCreateDTO);
         log.info("Cadastro atualizado com sucesso");
 
         return new ResponseEntity<>(pessoaDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/consultar-uma-pessoa")
+    public ResponseEntity<PessoaDTO> consultarUmaPessoa (Integer idPessoa) throws RegraDeNegocioException {
+        log.info("Consultando usuário");
+        PessoaDTO pessoaDTO = pessoaService.consultarUmaPessoa(idPessoa);
+        log.info("Consulta realizada com sucesso");
+
+        return new ResponseEntity<>(pessoaDTO, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/attornatus/pessoaapi/service/PessoaService.java
+++ src/main/java/com/attornatus/pessoaapi/service/PessoaService.java
@@ -3,6 +3,7 @@ package com.attornatus.pessoaapi.service;
 import com.attornatus.pessoaapi.dto.pessoadto.PessoaCreateDTO;
 import com.attornatus.pessoaapi.dto.pessoadto.PessoaDTO;
 import com.attornatus.pessoaapi.entities.PessoaEntity;
+import com.attornatus.pessoaapi.exception.RegraDeNegocioException;
 import com.attornatus.pessoaapi.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,7 @@ public class PessoaService {
         return converterEmPessoaDTO(pessoa);
     }
 
-    public PessoaDTO editarPessoa(Integer idPessoa, PessoaCreateDTO pessoaCreateDTO) {
+    public PessoaDTO editarPessoa(Integer idPessoa, PessoaCreateDTO pessoaCreateDTO) throws RegraDeNegocioException {
         PessoaEntity pessoa = buscarPessoaPorId(idPessoa);
 
         pessoa.setNome(pessoaCreateDTO.getNome());
@@ -34,8 +35,12 @@ public class PessoaService {
         return converterEmPessoaDTO(pessoa);
     }
 
-    public PessoaEntity buscarPessoaPorId (Integer idPessoa){
-        return pessoaRepository.findById(idPessoa).get();
+    public PessoaDTO consultarUmaPessoa (Integer idPessoa) throws RegraDeNegocioException {
+        return converterEmPessoaDTO(buscarPessoaPorId(idPessoa));
+    }
+
+    public PessoaEntity buscarPessoaPorId (Integer idPessoa) throws RegraDeNegocioException {
+        return pessoaRepository.findById(idPessoa).orElseThrow(() -> new RegraDeNegocioException("Não foi encontrado uma pessoa com o id " + idPessoa));
     }
 
     public PessoaEntity converterEmPessoaEntity (PessoaCreateDTO pessoa) {
