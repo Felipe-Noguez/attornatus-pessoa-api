23a5a0034bed0aa32ce1db5f0518c41dbc63f00e
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-02 12:11:36 -0300

    Script para inserir dados no banco H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+insert into pessoa
+values (1, '1998-02-25', 'Mike');
+
+insert into pessoa
+values (2, '1973-06-25', 'Roger');
+
+insert into pessoa
+values (3, '1989-04-25', 'James');
+
+insert into pessoa
+values (4, '1979-02-19', 'Neil');
+
+insert into ENDERECO
+values (5, '99999-333', 'Porto Alegre', 1, 'Rua X', 345, 0);
+
+insert into ENDERECO
+values (6, '99999-222', 'Porto Alegre', 1, 'Rua IX', 45, 1);
+
+insert into ENDERECO
+values (7, '88888-333', 'Porto Alegre', 2, 'Rua XV', 755, 0);
+
+insert into ENDERECO
+values (8, '88888-222', 'Porto Alegre', 2, 'Rua XVI', 679, 2);
+
+insert into ENDERECO
+values (9, '99999-243', 'Porto Alegre', 3, 'Rua 9', 432, 0);
+
+insert into ENDERECO
+values (2, '99543-333', 'Porto Alegre', 4, 'Rua 7', 345, 0);
