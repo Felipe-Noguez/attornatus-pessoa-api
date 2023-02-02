543b94c1ac8d6ccff8b45ebc4dc20d72e6262b4e
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-02 18:23:16 -0300

    Validações nos DTOs

--- src/main/java/com/attornatus/pessoaapi/dto/enderecodto/EnderecoCreateDTO.java
+++ src/main/java/com/attornatus/pessoaapi/dto/enderecodto/EnderecoCreateDTO.java
@@ -1,19 +1,35 @@
 package com.attornatus.pessoaapi.dto.enderecodto;
 
 import com.attornatus.pessoaapi.enums.TipoEndereco;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class EnderecoCreateDTO {
 
+    @Schema(description = "Id da pessoa para cadastrar o endereço", example = "4")
+    @Min(message = "Campo obrigatório, não pode ser menor que 1", value = 1)
     private Integer idPessoa;
+    @NotBlank(message = "Campo obrigatório, não pode estar em branco")
+    @Schema(description = "Campo com endereço", example = "Av. Bento Gonçalves")
     private String logradouro;
+    @NotBlank(message = "Campo obrigatório, não pode estar em branco")
+    @Schema(description = "Campo de cep", example = "98622-123")
     private String cep;
+    @Min(message = "Campo obrigatório, não pode ser menor que 1", value = 1)
+    @Schema(description = "Campo de número do imóvel", example = "897")
     private Integer numero;
+    @NotBlank(message = "Campo obrigatório, não pode estar em branco")
+    @Schema(description = "Campo com nome da cidade", example = "Porto Alegre")
     private String cidade;
+    @Schema(description = "Campo com tipo de endereço", example = "PRINCIPAL")
     private TipoEndereco tipoEndereco;
 }
--- src/main/java/com/attornatus/pessoaapi/dto/pessoadto/PessoaCreateDTO.java
+++ src/main/java/com/attornatus/pessoaapi/dto/pessoadto/PessoaCreateDTO.java
@@ -1,10 +1,13 @@
 package com.attornatus.pessoaapi.dto.pessoadto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -12,6 +15,10 @@ import java.time.LocalDate;
 @Data
 public class PessoaCreateDTO {
 
+    @NotBlank(message = "Campo obrigatório, não pode estar em branco")
+    @Schema(description = "Nome da pessoa", example = "Neil Peart")
     private String nome;
+    @NotNull(message = "Campo obrigatório, não pode estar em branco")
+    @Schema(description = "Data de nascimento da pessoa", example = "2000-01-01")
     private LocalDate dataNascimento;
 }
